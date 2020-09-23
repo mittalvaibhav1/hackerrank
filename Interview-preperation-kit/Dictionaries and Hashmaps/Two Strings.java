61070cfddf2fd8b478bb32b1bb81e42c5cdf8b72
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-09-23 15:02:33 +0530

    Two Strings

---
+++ Interview-preperation-kit/Dictionaries and Hashmaps/Two Strings.java
@@ -0,0 +1,50 @@
+//https://www.hackerrank.com/challenges/two-strings/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the twoStrings function below.
+    static String twoStrings(String s1, String s2) {
+        int index[] = new int[26];
+        for(char i : s1.toCharArray()) {
+            index[i-'a']++;
+        }
+        for(char i : s2.toCharArray()) {
+            if(index[i-'a']>0) {
+                return "YES";
+            }
+        }
+        return "NO";
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s1 = scanner.nextLine();
+
+            String s2 = scanner.nextLine();
+
+            String result = twoStrings(s1, s2);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
