86fd7705ae6c3fd36864e336bb05513a65ba942e
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-10-17 01:58:58 +0530

    Pairs

---
+++ Problems/Pairs.java
@@ -0,0 +1,58 @@
+//https://www.hackerrank.com/challenges/pairs/problem
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the pairs function below.
+    static int pairs(int k, int[] arr) {
+        int res = 0;
+        HashSet<Integer> set = new HashSet<>();
+        for(int i : arr) {
+            if(set.contains(k + i)){
+                res++;
+            }
+            if(set.contains(i - k)){
+                res++;
+            }
+            set.add(i);
+        }
+        return res;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int result = pairs(k, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
