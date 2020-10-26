e0528178cee60cbc544fb0b5a3380bcd368c022d
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-10-26 22:11:23 +0530

    Maximizing XOR O(1)

---
+++ Problems/Maximizing XOR O(1).java
@@ -0,0 +1,40 @@
+//https://www.hackerrank.com/challenges/maximizing-xor/problem
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the maximizingXor function below.
+    static int maximizingXor(int l, int r) {
+        int xor = (l ^ r);
+        int msb = 31 - Integer.numberOfLeadingZeros(xor);
+        int res = (1 << (msb + 1)) - 1;
+        return res;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int l = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int r = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int result = maximizingXor(l, r);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
